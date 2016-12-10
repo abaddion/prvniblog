6b67fb3bb2e644fa982eb963e82b73ad267c904e
Barush Balazikova <barush.balazikova@gmail.com>
2016-12-10 16:32:21 +0100

    blog s adminem

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from blog.models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# Create your views here.
